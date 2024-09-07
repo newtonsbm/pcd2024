7672272ea21447373d281bbcb75ec7dfd5e64058
Newton Miyoshi <newton.miyoshi@medway.com.br>
2024-09-07 01:11:59 -0300

    atividade a5 arquivos de confi docker compoes

--- material/compose/cafecompao/cafecompao/settings.py
+++ material/compose/cafecompao/cafecompao/settings.py
@@ -87,8 +87,13 @@ WSGI_APPLICATION = 'cafecompao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cafecompaobd',
+        'HOST': 'meubanquinho',
+        'PORT': '5432',
+        'USER': 'admin',
+        'PASSWORD': 'senha',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
